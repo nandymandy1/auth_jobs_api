482c2b2f04cd7e217fd475e87188f0c1b422d7ec
Nandy <nandymandy09@hotmail.com>
2020-04-08 22:42:16 +0530

    ENV Config added

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
 import dotenv from "dotenv";
 const { parsed } = dotenv.config();
-export const { APP_DB, PORT, APP_SECRET, REFRESH_TOKEN } = parsed;
+
+export const PORT = process.env.PORT || parsed.PORT;
+export const { APP_DB, APP_SECRET, REFRESH_TOKEN } = parsed;
